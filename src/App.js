669b7e3976eef2b3f7a92fedf0126e4a267e434c
Michele Gusmao <michele.gusmao7@gmail.com>
2022-06-20 18:09:15 -0300

    Adicionas pages e routes.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import './App.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 import Foods from './pages/Foods';
+import Drinks from './pages/Drinks';
+import FoodDetails from './pages/FoodDetails';
+import DrinkDetails from './pages/DrinkDetails';
+import FoodProgress from './pages/FoodProgress';
+import DrinkProgress from './pages/DrinkProgress';
+import Explorer from './pages/Explorer';
+import FoodExplore from './pages/FoodExplore';
+import DrinkExplore from './pages/DrinkExplore';
+import FoodsIngredients from './pages/FoodsIngredients';
+import DrinksIngredients from './pages/DrinksIngredients';
+import FoodsNationalities from './pages/FoodsNationalities';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function App() {
   return (
@@ -11,6 +25,20 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/foods" component={ Foods } />
+        <Route path="/drinks" component={ Drinks } />
+        <Route path="/foods/{id-da-receita}" component={ FoodDetails } />
+        <Route path="/drinks/{id-da-receita}" component={ DrinkDetails } />
+        <Route path="/foods/{id-da-receita}/in-progress" component={ FoodProgress } />
+        <Route path="/drinks/{id-da-receita}/in-progress" component={ DrinkProgress } />
+        <Route path="/explore" component={ Explorer } />
+        <Route path="/explore/foods" component={ FoodExplore } />
+        <Route path="/explore/drinks" component={ DrinkExplore } />
+        <Route path="/explore/foods/ingredients" component={ FoodsIngredients } />
+        <Route path="/explore/drinks/ingredients" component={ DrinksIngredients } />
+        <Route path="/explore/foods/nationalities" component={ FoodsNationalities } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/done-recipes" component={ DoneRecipes } />
+        <Route path="/favorite-recipes" component={ FavoriteRecipes } />
       </Switch>
     </div>
   );
---
+++ src/pages/FoodExplore.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function FoodExplore() {
+  return (
+    <div>FoodExplore</div>
+  );
+}
