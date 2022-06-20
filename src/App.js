06ac77fe812a022ad6a1cc024f9bde034627286a
Michele Gusmao <michele.gusmao7@gmail.com>
2022-06-20 15:31:34 -0300

    Commit inicial.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="meals">
-      <span className="logo">TRYBE</span>
+      <span className="logo">TRYBE - APP</span>
       <object
         className="rocksGlass"
         type="image/svg+xml"
         data={ rockGlass }
       >
-        Glass
+        Glass 
       </object>
     </div>
   );
