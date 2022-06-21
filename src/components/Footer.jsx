fd38497659c41d87366ca7ebbcf37b529222350e
juniorhaussler <juniorhaussler@gmail.com>
2022-06-21 14:40:34 -0300

    Requisito 20 concluído.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,28 +2,19 @@ import React from 'react';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import './Footer.css';
 
 export default function Footer() {
   return (
     <footer data-testid="footer" className="footer">
-      Footer
-      <button
-        type="button"
-        data-testid="drinks-bottom-btn"
-      >
-        <img src={ drinkIcon } alt="drink" />
+      <button type="button">
+        <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="drink" />
       </button>
-      <button
-        type="button"
-        data-testid="explore-bottom-btn"
-      >
-        <img src={ exploreIcon } alt="explore" />
+      <button type="button">
+        <img data-testid="explore-bottom-btn" src={ exploreIcon } alt="explore" />
       </button>
-      <button
-        type="button"
-        data-testid="food-bottom-btn"
-      >
-        <img src={ mealIcon } alt="meal" />
+      <button type="button">
+        <img data-testid="food-bottom-btn" src={ mealIcon } alt="meal" />
       </button>
     </footer>
 
