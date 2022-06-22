cd090a8902ad900d8308ff6d893faa6e71d8868b
Michele Gusmao <michele.gusmao7@gmail.com>
2022-06-22 09:56:09 -0300

    Telas Principais finalizadas. Requisitos do 25 ao 32.

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import CardsFood from '../components/CardsFood';
+import { fetchApiAllFoods,
+  fetchApiCategoriesFood, fetchApiFilterByCategory } from '../services/foodApi';
+import { addFood } from '../redux/actions';
 
 export default function Foods() {
   const foods = useSelector((state) => state.foodReducer.foods);
+  const dispatch = useDispatch();
+
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const callApiAndDispatch = async () => {
+    const result = await fetchApiAllFoods();
+    dispatch(addFood(result));
+  };
+
+  const callApiFoodCategories = async () => {
+    const numberMaxCategories = 4;
+    const result = await fetchApiCategoriesFood();
+    const categoriesList = result.filter((_, index) => index <= numberMaxCategories);
+    setCategories(categoriesList);
+  };
+
+  useEffect(() => {
+    callApiAndDispatch();
+    callApiFoodCategories();
+  }, []);
+
+  const filterByCategory = async ({ target: { value } }) => {
+    if (value !== selectedCategory) {
+      setSelectedCategory(value);
+      const result = await fetchApiFilterByCategory(value);
+      console.log(result);
+      dispatch(addFood(result));
+    } else {
+      setSelectedCategory('');
+      const result = await fetchApiAllFoods();
+      dispatch(addFood(result));
+    }
+  };
+
   return (
     <div>
       <Header
@@ -14,6 +52,25 @@ export default function Foods() {
         searchIcon
         typeFood="food"
       />
+      <button
+        type="button"
+        data-testid="All-category-filter"
+        value="All"
+        onClick={ callApiAndDispatch }
+      >
+        All
+      </button>
+      {categories.length > 0 && categories
+        .map(({ strCategory }) => (
+          <button
+            type="button"
+            key={ strCategory }
+            data-testid={ `${strCategory}-category-filter` }
+            value={ strCategory }
+            onClick={ filterByCategory }
+          >
+            { strCategory }
+          </button>))}
       <CardsFood cards={ foods } />
       <Footer />
     </div>
