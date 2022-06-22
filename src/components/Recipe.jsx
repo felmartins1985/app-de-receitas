606b2c821ecdf82f697b0c6aa96c7853212819ca
juniorhaussler <juniorhaussler@gmail.com>
2022-06-22 11:37:05 -0300

    Requisito 33 - Atualizado.

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -21,10 +21,10 @@ export default function Recipe() {
       <p data-testid="recipe-category">Category</p>
       <h3>Ingredients:</h3>
       <div className="div-ingredients">
-        <p data-testid={ `${index}-ingredient-name-and-measure` }>Ingredient1</p>
-        <p data-testid={ `${index}-ingredient-name-and-measure` }>Ingredient2</p>
-        <p data-testid={ `${index}-ingredient-name-and-measure` }>Ingredient3</p>
-        <p data-testid={ `${index}-ingredient-name-and-measure` }>Ingredient4</p>
+        <p data-testid="">Ingredient1</p>
+        <p data-testid="">Ingredient2</p>
+        <p data-testid="">Ingredient3</p>
+        <p data-testid="">Ingredient4</p>
       </div>
       <h3>Instructions:</h3>
       <div>
@@ -35,7 +35,7 @@ export default function Recipe() {
       </div>
       <div>
         <h2>RECOMMENDED</h2>
-        <p data-testid={ `${index}-recomendation-card` }>
+        <p data-testid="">
           AQUI FICAM OS CARDS COM AS RECEITAS RECOMENDADAS
         </p>
       </div>
--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Recipe from '../components/Recipe';
 
 export default function FoodDetails() {
   return (
     <div>
       FoodDetails
+      <Recipe />
     </div>
   );
 }
